d52abaaeb3f4ce835160eba417a059926cc9f1ae
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-03-25 11:16:41 +0530

    helia

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=4eb77d77-5f56-4dda-f21f-08dc43fde0c7&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        </script>
+      </body>
+    </Html>
+  );
+}
