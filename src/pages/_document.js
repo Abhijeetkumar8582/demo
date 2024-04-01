5a90d5446f3e89b26c7a8f9c1482a4a8bc7848d9
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-04-01 13:25:31 +0530

    Working on  the project

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=4eb77d77-5f56-4dda-f21f-08dc43fde0c7&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=2b10965e-bbd9-48bd-f22d-08dc43fde0c7&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
         </script>
       </body>
     </Html>
