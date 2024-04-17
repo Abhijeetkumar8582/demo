67662375227e3fe7eea2b432125f0a269b63cdb9
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-04-17 11:21:15 +0530

    Book room

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=19d41280-1f51-44b0-8789-08dc561bcfab&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=77e00fd4-d60f-46a5-9905-08dc53b2c1a6&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
         </script>
       </body>
     </Html>
